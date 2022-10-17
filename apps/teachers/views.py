ed47b2defa6ced5fa67478e7af7663692699abbd
Devendra116 <chauhandevendra116@gmail.com>
2022-10-17 16:22:33 +0530

    Added patch response for API

--- apps/teachers/views.py
+++ apps/teachers/views.py
@@ -17,3 +17,31 @@ class AssignmentsView(generics.ListCreateAPIView):
             data=self.serializer_class(assignments, many=True).data,
             status=status.HTTP_200_OK
         )
+
+    def patch(self, request, *args, **kwargs):
+        teacher = Teacher.objects.get(user=request.user)
+        request.data['teacher'] = teacher.id
+        state = Assignment.objects.get(pk=request.data['id'])
+        request.data['state']=state.state
+        request.data['assignment_teacher']=state.teacher.id
+        try:
+            assignment = Assignment.objects.get(pk=request.data['id'])
+        except Assignment.DoesNotExist:
+            return Response(
+                data={'error': 'Assignment does not exist/permission denied'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        serializer = self.serializer_class(assignment, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                data=serializer.data,
+                status=status.HTTP_200_OK
+            )
+
+        return Response(
+            data=serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
