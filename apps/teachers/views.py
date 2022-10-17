1b2f33584df57d3e9f4448d717d870da1dcc41cc
Devendra116 <chauhandevendra116@gmail.com>
2022-10-17 12:03:59 +0530

    Added get response for API

--- apps/teachers/views.py
+++ apps/teachers/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Teacher
+
+from apps.students.models import Assignment, Student
+from .serializers import TeacherAssignmentSerializer
+
+
+class AssignmentsView(generics.ListCreateAPIView):
+    serializer_class = TeacherAssignmentSerializer
+
+    def get(self, request, *args, **kwargs):
+        assignments = Assignment.objects.filter(teacher__user=request.user)
+
+        return Response(
+            data=self.serializer_class(assignments, many=True).data,
+            status=status.HTTP_200_OK
+        )
